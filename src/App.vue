a17f8c71c510ae6472e6ecc9f4678a63b80ca31a
juliot nuñez <nunezjuliot@gmail.com>
2023-06-16 02:39:12 -0500

    Initial commit
     Changes to be committed:
    	new file:   .eslintrc.cjs
    	new file:   .gitignore
    	new file:   .prettierrc.json
    	new file:   .vscode/extensions.json
    	new file:   README.md
    	new file:   env.d.ts
    	new file:   index.html
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   postcss.config.js
    	new file:   public/favicon.ico
    	new file:   src/App.vue
    	new file:   src/assets/base.css
    	new file:   src/assets/logo.svg
    	new file:   src/assets/main.css
    	new file:   src/components/Layouts/HeaderLayout.vue
    	new file:   src/components/icons/IconCommunity.vue
    	new file:   src/components/icons/IconDocumentation.vue
    	new file:   src/components/icons/IconEcosystem.vue
    	new file:   src/components/icons/IconSupport.vue
    	new file:   src/components/icons/IconTooling.vue
    	new file:   src/components/icons/chevron-right-svgrepo-com.svg
    	new file:   src/components/icons/search.svg
    	new file:   src/main.ts
    	new file:   src/stores/counter.ts
    	new file:   tailwind.config.js
    	new file:   tsconfig.app.json
    	new file:   tsconfig.json
    	new file:   tsconfig.node.json
    	new file:   vite.config.ts

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HeaderLayout from './components/Layouts/HeaderLayout.vue'
+</script>
+
+<template>
+  <header>
+    <HeaderLayout />
+  </header>
+
+  <main>
+  
+  </main>
+</template>
+
